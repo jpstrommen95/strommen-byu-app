47ff8d6a98bc9ac72d487106ae1efce5db6eeede
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-05-20 16:09:15 -0600

    add deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,32 @@
+// A helper script for `npm run build`.
+// isolated usage: `node ./scripts/deploy.js`
+
+const fs = require('fs');
+const archiver = require('archiver');
+const path = require('path');
+
+const sourceDir = path.join(__dirname, '../build');
+const outputPath = path.join(__dirname, 'output/public_html.zip');
+
+const main = async () => {
+  console.log('Starting deploy script.');
+  console.log(`Zipping ${sourceDir} to ${outputPath}`);
+
+  // Create a file to stream archive data to
+  const writeStream = fs.createWriteStream(outputPath);
+  const archive = archiver('zip', { zlib: { level: 9 } }); // Set compression level
+
+  // Set up listeners
+  writeStream.on('close', () => {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('Archiver has been finalized and the output file descriptor has closed.');
+  });
+  archive.on('error', (err) => { throw err; });
+
+  // Finalize
+  archive.pipe(writeStream);
+  archive.directory(sourceDir, false);
+  archive.finalize();
+};
+
+(async () => (await main()))();
