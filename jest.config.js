7fd4c3b020b8b2e17d8348a6421688d51db75087
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-05-22 14:54:44 -0600

    jest test set up

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/__mocks__/fileMock.js',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/serviceWorker.js',
+    '!src/setupTests.js',
+  ],
+  coverageReporters: ['html', 'text'],
+};
