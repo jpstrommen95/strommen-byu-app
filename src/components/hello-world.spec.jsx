fe281e32083fbfb4276d87a100f884344f558401
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-05-23 18:09:49 -0600

    fix test import

--- src/components/hello-world.spec.jsx
+++ src/components/hello-world.spec.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { test, expect } from 'jest';
+import { it, expect } from '@jest/globals';
 import HelloWorld from './hello-world';
 
-test('renders Hello, World! message', () => {
+it('renders Hello, World! message', () => {
   render(<HelloWorld />);
   const headingElement = screen.getByText(/Hello, World!/i);
   expect(headingElement).toBeInTheDocument();
