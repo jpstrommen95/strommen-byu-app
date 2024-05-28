c173da35954ec7432444abf7d2aa2de16e562793
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-05-28 14:27:51 -0600

    sort package json

--- src/setup-tests.js
+++ src/setup-tests.js
@@ -1,2 +1,3 @@
 // Sets up testing environment to support `test:ci` command through custom `jest.config.js` config
+/* eslint-disable-next-line import/no-extraneous-dependencies */ // setup-tests isn't in build
 require('@testing-library/jest-dom');
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,3 @@
 // Sets up testing environment to support `npm run test` through default `react-scripts` config
+/* eslint-disable-next-line import/no-extraneous-dependencies */ // setupTests isn't in build
 import '@testing-library/jest-dom';
