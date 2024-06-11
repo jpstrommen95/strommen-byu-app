9fc25dda13796a7d41808f1b09b2e6f0bc1b210d
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-06-11 16:33:22 -0600

    feat(commit script): add ci/cd option (just a stub)

--- commit-build-to-asset-repo.js
+++ commit-build-to-asset-repo.js
@@ -12,7 +12,13 @@ const semVerRegex = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*
 const argv = yargs(hideBin(process.argv))
   .usage('Copies and commits files from source code repo to the appropriate hosting-assets repo.')
   .example('node ./commit-build-to-repo-b-copy.js --help', 'Display help.')
-  .example('node ./commit-build-to-repo-b-copy.js -s dev -v 1.0.0', 'Typical usage.')
+  .example('node ./commit-build-to-repo-b-copy.js -s dev -v 1.0.0', 'Local usage.')
+  .example('node ./commit-build-to-repo-b-copy.js -s dev -v 1.0.0 -c', 'CI/CD usage.')
+  .option('isCiCd', {
+    alias: 'c',
+    type: 'boolean',
+    description: 'Set the flag (no value needed) if running on a ci/cd platform.',
+  })
   .option('stage', {
     alias: 's',
     required: true,
@@ -95,12 +101,21 @@ function clearDestRepo({ hostingAssetRepoDir }) {
 }
 
 function main() {
-  const { stage, version } = argv;
+  const {
+    c: isCiCd, // yargs is acting weird, only the alias 'c' seems to destructure properly
+    stage,
+    version,
+  } = argv;
   try {
-    console.log(`Commencing script to commit ${stage} ${version} build.`);
+    console.log(`Commencing script to commit ${stage} ${version} build ${isCiCd ? 'with' : 'without'} CI/CD.`);
     const { srcCodeRepoDir, hostingAssetRepoDir } = getRepoPaths({ stage });
-    console.log('Validating repos...');
-    validateRepos({ srcCodeRepoDir, hostingAssetRepoDir });
+    if (isCiCd) {
+      console.log('Cloning repo...');
+      // TODO actually do that, refactor to use parent directory too
+    } else {
+      console.log('Validating repos...');
+      validateRepos({ srcCodeRepoDir, hostingAssetRepoDir });
+    }
     console.log('Clearing destination repo...');
     clearDestRepo({ hostingAssetRepoDir });
     console.log('Copying build directory contents from source code repo to hosting assets repo...');
