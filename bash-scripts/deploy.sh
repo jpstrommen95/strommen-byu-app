ce6df5a5e7fc113515e9931ba4a66ad9ec03f9d0
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-06-14 14:08:21 -0600

    fix(circle ci): follow documented best shell practices from circleci

--- bash-scripts/deploy.sh
+++ bash-scripts/deploy.sh
@@ -1,4 +1,16 @@
+#!/usr/bin/env bash
+
 # usage: `./bash-scripts/deploy.sh`
+# ci usage: `sh ./bash-scripts/deploy.sh`
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
 
 STROMMEN_BYU_APP_VERSION=$(node ./node-scripts/print-p-json-version.js)
 STROMMEN_BYU_GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
