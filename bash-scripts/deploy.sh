8fbfd4b7fc97f60502337e2a4f275f6f3a3baae6
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-06-17 15:13:47 -0600

    build(deploy script): completely use git as branch source of truth (#19)

--- bash-scripts/deploy.sh
+++ bash-scripts/deploy.sh
@@ -4,7 +4,7 @@
 # ci usage: `sudo ./bash-scripts/deploy.sh`
 
 # Exit script if you try to use an uninitialized variable.
-set -o nounset # ignore safeguard to support local usage, as CIRCLE_BRANCH is unset
+set -o nounset
 
 # Exit script if a statement returns a non-true return value.
 set -o errexit
@@ -12,17 +12,8 @@ set -o errexit
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
-STROMMEN_BYU_APP_VERSION=$(node ./node-scripts/print-p-json-version.js)
 STROMMEN_BYU_GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-BRANCH="no branch"
+STROMMEN_BYU_APP_VERSION=$(node ./node-scripts/print-p-json-version.js)
 
-if [ -z "$CIRCLE_BRANCH" ]; then
-  echo "CircleCI branch is null, using git instead."
-  BRANCH=${STROMMEN_BYU_GIT_BRANCH}
-else
-  echo "Branch detected from CircleCI."
-  BRANCH=${CIRCLE_BRANCH}
-fi
-
-echo "Fake deploying from branch: $BRANCH!"
+echo "Fake deploying from branch: $STROMMEN_BYU_GIT_BRANCH!"
 echo "Fake deploying as version: $STROMMEN_BYU_APP_VERSION!"
