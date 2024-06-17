2647d29650c85c517dde2a6c3a0d54d70a059a0c
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-06-17 14:53:58 -0600

    fix(deploy.sh): init branch var

--- bash-scripts/deploy.sh
+++ bash-scripts/deploy.sh
@@ -4,7 +4,7 @@
 # ci usage: `sh ./bash-scripts/deploy.sh`
 
 # Exit script if you try to use an uninitialized variable.
-set -o nounset
+set -o nounset # ignore safeguard to support local usage, as CIRCLE_BRANCH is unset
 
 # Exit script if a statement returns a non-true return value.
 set -o errexit
@@ -14,6 +14,7 @@ set -o pipefail
 
 STROMMEN_BYU_APP_VERSION=$(node ./node-scripts/print-p-json-version.js)
 STROMMEN_BYU_GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH="no branch"
 
 if [ -z "$CIRCLE_BRANCH" ]; then
   echo "CircleCI branch is null, using git instead."
