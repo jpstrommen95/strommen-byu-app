e0fe4b86bc241c8106473450cbf7e6d341163191
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-07-03 14:00:47 -0600

    feat(app): add app component

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Game from './game';
+import TopMenu from './top-menu';
+
+function App() {
+  return (
+    <div>
+      <TopMenu />
+      <Game />
+      {' '}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/app.spec.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './app';
+
+function mockApp() {
+  return <App />;
+}
+
+describe('app', () => {
+  it('should render the top menu', () => {
+    expect.assertions(1);
+    // WHEN
+    render(mockApp());
+    const topMenuElement = screen.getByRole('heading', /strommenbyu.com/i);
+
+    // THEN
+    expect(topMenuElement).toBeInTheDocument();
+  });
+});
