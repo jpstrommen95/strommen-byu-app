1a102cdb4814f4ed2e9ca7fe5eba26127c31035e
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-07-03 14:12:39 -0600

    feat(menu): add linked in link

--- src/components/top-menu.jsx
+++ src/components/top-menu.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 function TopMenu() {
   return (
     <div className="top-menu">
-      <h1 className="display-1">StrommenByu.com</h1>
+      <h1 className="display-1">
+        StrommenByu.com
+      </h1>
+      <a
+        className="link"
+        href="https://www.linkedin.com/in/justin-strommen-7563ba74"
+      >
+        LinkedIn
+      </a>
     </div>
   );
 }
