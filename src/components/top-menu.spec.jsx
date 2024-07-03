143060799544feed315d790e9e36378214be6cfe
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-07-03 16:14:59 -0600

    test(menu): fix screen query

--- src/components/top-menu.spec.jsx
+++ src/components/top-menu.spec.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import {
   it, describe, expect,
 } from '@jest/globals';
@@ -10,23 +10,36 @@ function mockTopMenu() {
 }
 
 describe('top menu', () => {
-  it('should render the top menu', () => {
-    expect.assertions(1);
+  it('should render the app title text', () => {
+    expect.assertions(3);
     // WHEN
     render(mockTopMenu());
-    const topMenuElement = screen.getByRole('heading', /strommenbyu.com/i);
+    const topMenuElement = screen.queryByText(/strommenbyu.com/i);
 
     // THEN
     expect(topMenuElement).toBeInTheDocument();
+    expect(topMenuElement).toHaveClass('display-1');
+    expect(topMenuElement).toHaveClass('top-menu-name');
   });
 
-  it('should contain the app title in the menu', () => {
+  it('should render a link to linkedin', () => {
+    expect.assertions(2);
+    // WHEN
+    render(mockTopMenu());
+    const htmlElement = screen.queryByRole('link', /linkedin/i);
+
+    // THEN
+    expect(htmlElement).toBeInTheDocument();
+    expect(htmlElement).toHaveClass('label-2');
+  });
+
+  it('should contain a link to a linkedin page', () => {
     expect.assertions(1);
     // WHEN
     render(mockTopMenu());
-    const topMenuElement = screen.queryByRole('div');
+    const htmlElement = screen.queryByRole('link', /linkedin/i);
 
     // THEN
-    expect(topMenuElement).toBeInTheDocument();
+    expect(htmlElement.href).toMatch(/linkedin.com.*justin.*strommen/i);
   });
 });
