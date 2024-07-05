068900da3c454bf582dcf619f969955e72c923d5
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-07-05 16:39:44 -0600

    feat(nav): apply style to side-nav-rail

--- src/components/side-nav-rail.jsx
+++ src/components/side-nav-rail.jsx
@@ -3,8 +3,15 @@ import PropTypes from 'prop-types';
 
 function SideNavRail({ pageArray }) {
   return (
-    <div>
-      {pageArray.map(({ name }) => <h1 key={name}>{name}</h1>)}
+    <div className="side-nav-rail">
+      {pageArray.map(({ name }) => (
+        <h1
+          className="label-1 side-nav-rail-name"
+          key={name}
+        >
+          {name}
+        </h1>
+      ))}
     </div>
   );
 }
