294046c6c82c51108238fbe5a81665880635ff41
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-07-10 14:21:33 -0600

    fix(version info): derive real version and stage

--- src/components/version-info.jsx
+++ src/components/version-info.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import pjson from '../../package.json';
+
+function urlToStage() {
+  const url = window.location.href;
+  if (/^https?:\/\/localhost/.test(url)) {
+    return 'local';
+  }
+
+  if (/^https?:\/\/dev.strommenbyu.com/.test(url)) {
+    return 'dev';
+  }
+
+  if (/^https?:\/\/strommenbyu.com/.test(url)) {
+    return 'prod';
+  }
+
+  return 'unknown';
+}
 
 function VersionInfo() {
   return (
     <div>
       <h1>Version</h1>
-      <p>v0.0.0</p>
-      <p>dev</p>
+      <p>{`v${pjson.version}`}</p>
+      <p>{urlToStage()}</p>
     </div>
   );
 }
--- src/components/version-info.spec.jsx
+++ src/components/version-info.spec.jsx
@@ -8,6 +8,7 @@ import VersionInfo from './version-info';
 
 /** (Note the prepended v, but otherwise:) From semver.org, see also https://regex101.com/r/vkijKf/1/. */
 const semVerRegex = /^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
+const stageRegex = /(local|dev|prod|unknown)/i;
 
 function mockComponent() {
   return (
@@ -17,15 +18,54 @@ function mockComponent() {
   );
 }
 
+function setupLocation({ href }) {
+  delete window.location;
+  window.location = {
+    href,
+  };
+}
+
 describe('version info', () => {
   it('renders the component\'s info', () => {
     expect.assertions(3);
     render(mockComponent());
     const titleElement = screen.queryByRole('heading', { name: /version/i });
     const versionElement = screen.queryByText(semVerRegex);
-    const stageElement = screen.queryByText(/dev/i);
+    const stageElement = screen.queryByText(stageRegex);
     expect(titleElement).toBeInTheDocument();
     expect(versionElement).toBeInTheDocument();
     expect(stageElement).toBeInTheDocument();
+  });
+
+  it('should identify a local env', () => {
+    expect.assertions(1);
+    setupLocation({ href: 'https://localhost:1234' });
+    render(mockComponent());
+    const stageElement = screen.queryByText(stageRegex);
+    expect(stageElement).toHaveTextContent('local');
+  });
+
+  it('should identify a dev env', () => {
+    expect.assertions(1);
+    setupLocation({ href: 'https://dev.strommenbyu.com/about-me' });
+    render(mockComponent());
+    const stageElement = screen.queryByText(stageRegex);
+    expect(stageElement).toHaveTextContent('dev');
+  });
+
+  it('should identify a prod env', () => {
+    expect.assertions(1);
+    setupLocation({ href: 'https://strommenbyu.com/about-me?abc=true' });
+    render(mockComponent());
+    const stageElement = screen.queryByText(stageRegex);
+    expect(stageElement).toHaveTextContent('prod');
+  });
+
+  it('should identify an unknown env', () => {
+    expect.assertions(1);
+    setupLocation({ href: 'example.com' });
+    render(mockComponent());
+    const stageElement = screen.queryByText(stageRegex);
+    expect(stageElement).toHaveTextContent('unknown');
   });
 });
