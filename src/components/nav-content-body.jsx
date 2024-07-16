63c18f5abb06906af43a06e56ad1221dfe6175d6
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-07-16 16:02:14 -0600

    feat(nav): add nav content body

---
+++ src/components/nav-content-body.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import PageContent from './page-content';
+import SideNavRail from './side-nav-rail';
+
+function NavContentBody({ component, pageArray }) {
+  return (
+    <div className="nav-content-body">
+      <SideNavRail pageArray={pageArray} />
+      <PageContent component={component} />
+    </div>
+  );
+}
+
+NavContentBody.propTypes = {
+  component: PropTypes.shape({}).isRequired,
+  pageArray: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    routeTo: PropTypes.string,
+  })).isRequired,
+};
+
+export default NavContentBody;
