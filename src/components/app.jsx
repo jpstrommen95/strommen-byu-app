2e0694be0a3bf955e83cad2bff81dec192fa7c7e
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-07-16 16:48:54 -0600

    feat(app): apply nav content body

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Game from './game';
 import TopMenu from './top-menu';
-import SideNavRail from './side-nav-rail';
 import VersionInfo from './version-info';
-import PageContent from './page-content';
 import AboutMeOverview from './about-me-overview';
+import NavContentBody from './nav-content-body';
 
 function App() {
   return (
     <div className="app">
       <TopMenu />
-      <SideNavRail
+      <NavContentBody
         pageArray={[
           {
             name: 'About Me',
@@ -26,8 +25,6 @@ function App() {
             routeTo: '/version',
           },
         ]}
-      />
-      <PageContent
         component={(
           <Routes>
             <Route path="/" element={<AboutMeOverview />} />
