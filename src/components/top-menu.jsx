16959a7ae37a3cc2e43d6d76d950b609770d1304
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2024-08-01 22:26:11 -0600

    feat(top menu): add cv link

--- src/components/top-menu.jsx
+++ src/components/top-menu.jsx
@@ -22,6 +22,14 @@ function TopMenu() {
       >
         LinkedIn
       </a>
+      <a
+        className="link label-2"
+        href="https://docs.google.com/document/d/1gdMGqxt4xQ2wO9bin61Q3-OdAIHB0B-3zxw3-rifxn0/edit?usp=sharing"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Career CV
+      </a>
     </div>
   );
 }
