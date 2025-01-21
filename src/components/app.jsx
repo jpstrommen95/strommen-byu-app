ce90e9fc3d635631272602d9f50b09748010fa8e
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2025-01-21 15:41:04 -0700

    feat(fishers): add separate component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Fishers from './fishers';
 import Game from './game';
 import TopMenu from './top-menu';
 import VersionInfo from './version-info';
@@ -39,7 +40,7 @@ function App() {
           <Routes>
             <Route path="/" element={<AboutMeOverview />} />
             <Route path="/about-me" element={<AboutMeOverview />} />
-            <Route path="/fishers" element={<PageContent component={<h1>Fishers</h1>} />} />
+            <Route path="/fishers" element={<PageContent component={<Fishers />} />} />
             <Route path="/games" element={<PageContent component={<Game />} />} />
             <Route path="/i-made-that-up.org" element={<PageContent component={<Relationships />} />} />
             <Route path="/i-made-that-up" element={<PageContent component={<Relationships />} />} />
---
+++ src/components/fishers.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Fishers() {
+  return (
+    <div>
+      <h1>Fishers 2nd Ward Meetinghouse Cleanup</h1>
+      <p>
+        It is time for our 2nd Ward families to sign up
+        for our meetinghouse cleaning days in February.
+        I&apos;ve prepared a SignUp page for each Saturday in February.
+        Please select 1 or 2 days for your family to participate in cleaning the church.
+        I feel that using this approach lets you choose your timing according
+        to your schedule and let&apos;s you know which task you will perform that day.
+      </p>
+    </div>
+  );
+}
+
+export default Fishers;
