4596c51cb38d748dde301d7c00754aa86c0d5c98
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2025-01-21 16:31:37 -0700

    test(fishers): add tests

--- src/components/fishers.spec.jsx
+++ src/components/fishers.spec.jsx
@@ -1,1 +1,21 @@
-// TODO
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import {
+  it, expect, describe,
+} from '@jest/globals';
+import Fishers from './fishers';
+
+function mockComponent() {
+  return (
+    <Fishers />
+  );
+}
+
+describe('fishers', () => {
+  it('renders the component', () => {
+    expect.assertions(1);
+    render(mockComponent());
+    const htmlElement = screen.queryByRole('heading', { name: /fishers 2nd ward meetinghouse cleanup/i });
+    expect(htmlElement).toBeInTheDocument();
+  });
+});
